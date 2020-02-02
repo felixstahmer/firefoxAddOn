47ccb08b25dccdee395126f5a2283d320404210b
felixstahmer <felix.stahmer@t-online.de>
2020-02-02 13:48:53 +0100

    simple web extension

---
+++ basecamp-Add-On/requests/background.js
@@ -0,0 +1,19 @@
+var pattern = "https://mdn.mozillademos.org/*";
+
+function redirect(requestDetails) {
+  console.log("Redirecting: " + requestDetails.url);
+  return {
+    redirectUrl: "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif"
+  };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  redirect,
+  {urls:[pattern], types:["image"]},
+  ["blocking"]
+);
+
+
+
+window.onload(
+    console.log("hello"))
